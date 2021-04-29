708ba7d805d2519049fea80d7f403780b46aadef
jneilliii <jneilliii+github@gmail.com>
2021-04-29 16:36:28 -0400

    add settings interface to allow port configuration, defaults to 80

--- octoprint_gridspace/__init__.py
+++ octoprint_gridspace/__init__.py
@@ -9,10 +9,10 @@ import hashlib
 import logging
 
 try:
-    # noinspection PyCompatibility
-    from urllib.parse import urlencode
+	# noinspection PyCompatibility
+	from urllib.parse import urlencode
 except ImportError:
-    from urllib import urlencode
+	from urllib import urlencode
 
 # noinspection PyCompatibility
 import concurrent.futures
@@ -33,157 +33,171 @@ from requests.exceptions import Timeout
 from requests.exceptions import HTTPError
 from requests.exceptions import ConnectionError
 
-def background_spool(file_saver, get_name, ip_addr, logger, version):
-    count = 0
-    while True:
-        logger.debug('v{} connecting'.format(version))
-        try:
-            uuid = socket.getfqdn()
-            host = socket.gethostname()
-            name = get_name() or host
-            addr = ip_addr or str(socket.gethostbyname(uuid))
-            stat = {"device":{
-                        "name":name,
-                        "host":host,
-                        "uuid":uuid,
-                        "port":80,
-                        "mode":"octo",
-                        "addr":[addr]
-                    },
-                    "state":"ready",
-                    "rand":round(time.time()),
-                    "type":"op-{}".format(version)}
-            if count < 2:
-                logger.info('connect {} = [{}] {}'.format(count,uuid,stat))
-            else:
-                logger.debug('connect {} = [{}] {}'.format(count,uuid,stat))
-            stat = urllib.parse.quote_plus(json.dumps(stat, separators=(',', ':')))
-            url = "https://live.grid.space/api/grid_up?uuid={uuid}&stat={stat}".format(uuid=uuid,stat=stat)
-            response = requests.get(url)
-        except ConnectionError as error:
-            logger.info('connection error {}'.format(error))
-            time.sleep(10)
-            break
-        except HTTPError as error:
-            logger.info('http error {}'.format(error))
-            time.sleep(5)
-            break
-        except Timeout:
-            logger.info('connection timeout')
-            time.sleep(1)
-            continue
-        finally:
-            count = count + 1
 
-        if response:
-            text = response.text
-            if count < 2:
-                logger.info('response = {}'.format(text))
-            if text == 'superceded':
-                logger.info('superceded')
-                break
-            elif text == 'reconnect':
-                logger.debug('reconnect')
-            else:
-                body = text.split('\0')
-                file = body[0]
-                gcode = body[1]
-                logger.info('received "{}" length={}'.format(file,len(gcode)))
-                file_saver(file, gcode)
+def background_spool(file_saver, get_name, ip_addr, port, logger, version):
+	count = 0
+	while True:
+		logger.debug('v{} connecting'.format(version))
+		try:
+			uuid = socket.getfqdn()
+			host = socket.gethostname()
+			name = get_name() or host
+			addr = ip_addr or str(socket.gethostbyname(uuid))
+			stat = {"device": {
+				"name": name,
+				"host": host,
+				"uuid": uuid,
+				"port": port,
+				"mode": "octo",
+				"addr": [addr]
+			},
+				"state": "ready",
+				"rand": round(time.time()),
+				"type": "op-{}".format(version)}
+			if count < 2:
+				logger.info('connect {} = [{}] {}'.format(count, uuid, stat))
+			else:
+				logger.debug('connect {} = [{}] {}'.format(count, uuid, stat))
+			stat = urllib.parse.quote_plus(json.dumps(stat, separators=(',', ':')))
+			url = "https://live.grid.space/api/grid_up?uuid={uuid}&stat={stat}".format(uuid=uuid, stat=stat)
+			response = requests.get(url)
+		except ConnectionError as error:
+			logger.info('connection error {}'.format(error))
+			time.sleep(10)
+			break
+		except HTTPError as error:
+			logger.info('http error {}'.format(error))
+			time.sleep(5)
+			break
+		except Timeout:
+			logger.info('connection timeout')
+			time.sleep(1)
+			continue
+		finally:
+			count = count + 1
+
+		if response:
+			text = response.text
+			if count < 2:
+				logger.info('response = {}'.format(text))
+			if text == 'superceded':
+				logger.info('superceded')
+				break
+			elif text == 'reconnect':
+				logger.debug('reconnect')
+			else:
+				body = text.split('\0')
+				file = body[0]
+				gcode = body[1]
+				logger.info('received "{}" length={}'.format(file, len(gcode)))
+				file_saver(file, gcode)
+
 
 class FileSaveWrapper:
-    def __init__(self, gcode):
-        self.gcode = gcode
+	def __init__(self, gcode):
+		self.gcode = gcode
 
-    def save(self, destination):
-        f = open(destination, "w")
-        f.write(self.gcode)
-        f.close()
+	def save(self, destination):
+		f = open(destination, "w")
+		f.write(self.gcode)
+		f.close()
+
 
 class GridspacePlugin(octoprint.plugin.SettingsPlugin,
-                      octoprint.plugin.StartupPlugin,
-                      octoprint.plugin.EnvironmentDetectionPlugin,
-                      octoprint.plugin.TemplatePlugin,
-                      octoprint.plugin.AssetPlugin):
+					  octoprint.plugin.StartupPlugin,
+					  octoprint.plugin.EnvironmentDetectionPlugin,
+					  octoprint.plugin.TemplatePlugin,
+					  octoprint.plugin.AssetPlugin):
 
-    def __init__(self):
-        self._start_time = monotonic_time()
+	def __init__(self):
+		self._start_time = monotonic_time()
 
-    def initialize(self):
-        logger = self._logger
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.connect(('8.8.8.8', 80))
-            ip_addr = sock.getsockname()[0]
-            self._ip_addr = ip_addr
-            logger.info('ip_addr = {}'.format(ip_addr))
-        except socket.error:
-            logger.info('ip_addr error')
-        finally:
-            sock.close()
+	def initialize(self):
+		logger = self._logger
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			sock.connect(('8.8.8.8', 80))
+			ip_addr = sock.getsockname()[0]
+			self._ip_addr = ip_addr
+			logger.info('ip_addr = {}'.format(ip_addr))
+		except socket.error:
+			logger.info('ip_addr error')
+		finally:
+			sock.close()
 
-    def file_saver(self, filename, gcode):
-        self._file_manager.add_file("local", filename, FileSaveWrapper(gcode))
+	def file_saver(self, filename, gcode):
+		self._file_manager.add_file("local", filename, FileSaveWrapper(gcode))
 
-    def get_name(self):
-        return self._settings.global_get(["appearance", "name"])
+	def get_name(self):
+		return self._settings.global_get(["appearance", "name"])
 
-    def get_settings_defaults(self):
-        return dict(enabled=None,appearance=dict(name="aname"))
+	def get_settings_defaults(self):
+		return dict(enabled=None, appearance=dict(name="aname"), port=80)
 
-    def on_settings_save(self, data):
-        self._logger.debug('settings_save')
+	def on_settings_save(self, data):
+		old_port = self._settings.get_int(["port"])
+		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+		new_port = self._settings.get_int(["port"])
+		if old_port != new_port:
+			self._logger.debug("port changed to {} reinitializing".format(data["port"]))
+			self.on_after_startup()
 
-    def on_environment_detected(self, environment, *args, **kwargs):
-        self._environment = environment
+	def on_environment_detected(self, environment, *args, **kwargs):
+		self._environment = environment
 
-    def get_assets(self):
-        return dict(css=["css/gridspace.css"],
-                    js=["js/gridspace.js"])
+	def get_assets(self):
+		return dict(css=["css/gridspace.css"],
+					js=["js/gridspace.js"])
 
-    def on_after_startup(self):
-        thread = threading.Thread(target=background_spool, kwargs=({
-            "file_saver": self.file_saver,
-            "get_name": self.get_name,
-            "ip_addr": self._ip_addr,
-            "logger": self._logger,
-            "version": self._plugin_version
-        }))
-        thread.daemon = True
-        thread.start()
-        self._thread = thread
+	def get_template_configs(self):
+		return [dict(type="settings", custom_bindings=False)]
 
-    def on_event(self, event, payload):
-        self._logger.debug('event {} {}'.format(event,payload))
+	def on_after_startup(self):
+		thread = threading.Thread(target=background_spool, kwargs=({
+			"file_saver": self.file_saver,
+			"get_name": self.get_name,
+			"ip_addr": self._ip_addr,
+			"port": self._settings.get_int(["port"]),
+			"logger": self._logger,
+			"version": self._plugin_version
+		}))
+		thread.daemon = True
+		thread.start()
+		self._thread = thread
 
-    def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-        # for details.
-        return dict(
-            gridspace=dict(
-                displayName="GridSpace Plugin",
-                displayVersion=self._plugin_version,
+	def on_event(self, event, payload):
+		self._logger.debug('event {} {}'.format(event, payload))
 
-                # version check: github repository
-                type="github_release",
-                user="GridSpace",
-                repo="OctoPrint-GridSpace",
-                current=self._plugin_version,
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+		# for details.
+		return dict(
+			gridspace=dict(
+				displayName="GridSpace Plugin",
+				displayVersion=self._plugin_version,
 
-                # update method: pip
-                pip="https://github.com/GridSpace/OctoPrint-GridSpace/archive/{target_version}.zip"
-            )
-        )
+				# version check: github repository
+				type="github_release",
+				user="GridSpace",
+				repo="OctoPrint-GridSpace",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/GridSpace/OctoPrint-GridSpace/archive/{target_version}.zip"
+			)
+		)
+
 
 __plugin_name__ = "GridSpace Plugin"
 __plugin_pythoncompat__ = ">=3,<4"
 
-def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = GridspacePlugin()
 
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = GridspacePlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
